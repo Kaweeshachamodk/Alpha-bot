d07eecef6ba7d315ea157d37c80f04286ade8d3c
kaweeshachamod <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-08-14 19:50:06 +0530

    New logo version 🐺2.3.4

--- plugins/ichu.js
+++ plugins/ichu.js
@@ -12,7 +12,7 @@ const lol = "need some word"
 
     if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'sophia ?(.*)', fromMe: true,dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'alphax ?(.*)', fromMe: true,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(lol);
 
@@ -25,7 +25,7 @@ const lol = "need some word"
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'ttlogo ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'alphax ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(lol);
 
